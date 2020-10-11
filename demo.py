18ab80675fb2071d9f5cfaacec57f49d04400ca2
meichenli <li1290@purdue.edu>
2020-10-11 17:02:48 -0400

    GPIO

--- demo.py
+++ demo.py
@@ -1,2 +1,17 @@
-print('млшаэ')
-print('llalalalala')
+import RPi.GPIO as  GPIO
+import time
+
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel,GPIO.IN)
+
+def callback(channel):
+    if GPIO.input(channel):
+        print("Null")
+    else:
+        print("water")
+
+GPIO.add_event_detect(channel,GPIO.BOTH,bouncetime=300)
+GPIO.add_event_callback(channel,callback)
+while(1):
+    time.slepp(1)

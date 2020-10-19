17106c79302d71a2cee81e1ba338f81e8a204290
MeichenLi <li2190@purdue.edu>
2020-10-19 16:41:08 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -45,7 +45,7 @@ def runController():
 
 while True:
   runController()
-  level = interp(output, [0, 1023], [100, 0])
+  level = interp(level, [0, 1023], [100, 0])
   print("Moisture:", int(level))
   print("Moisture: {0:0.1f}".format(level))
   sleep(0.1)

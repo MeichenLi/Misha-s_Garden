574e2485b5009a1a062e4777328202099c4947da
MeichenLi <li2190@purdue.edu>
2020-10-19 12:45:46 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,20 +1,41 @@
-import RPi.GPIO as  GPIO
+import RPi.GPIO as GPIO
 import time
+import spidev
 
-pin = 21
+# Moisture sensor channel on MCP3008
+moisture_channel = 0
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin,GPIO.IN)
+TRIGGER_PIN = 18
+threshold = 10
 
-def callback(pin):
-    if GPIO.input(pin):
-        print("dry") # no water,big resistance, value = 1
-        time.sleep(1)
+# Open SPI bus
+spi = spidev.SpiDev()
+spi.open(0, 0)
+
+# Function to read SPI data from MCP3008 chip
+def ReadChannel(channel):
+    adc = spi.xfer2([1, (8+channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
+
+# Function to read sensor connected to MCP3008
+def readMoisture():
+    level = ReadChannel(moisture_channel)
+    return level
+
+# Controller main function
+def runController():
+    level = readMoisture()
+
+# Check moisture.format(level)
+    if (level < threshold):
+        GPIO.output(TRIGGER_PIN, True)
     else:
-        print("watery")
-        time.sleep(1)
+        GPIO.output(TRIGGER_PIN, False)
 
-GPIO.add_event_detect(pin,GPIO.BOTH,bouncetime=300)
-GPIO.add_event_callback(pin,callback)
+    print("Moisture: {0:0.1f}".format(level))
 
 while True:
-    time.sleep(1)
+    runController()
+    time.sleep(10)
